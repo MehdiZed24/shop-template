89b1680c387a6fbe060a251bf78a53fcc37d4f43
MehdiZed24 <mehdiziani1989@gmail.com>
2021-10-03 10:53:19 +0200

    Début du Register Form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegisterController extends AbstractController
 {
-    /**
-     * @Route("/inscription", name="register")
-     */
-    public function index(): Response
-    {
-        return $this->render('register/index.html.twig');
-    }
+  /**
+   * @Route("/inscription", name="register")
+   */
+  public function index(): Response
+  {
+
+    $user = new User();
+    $form = $this->createForm(RegisterType::class, $user);
+
+    return $this->render('register/index.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
