6c784683e7196096b31120ad866d19283f790b55
MehdiZed24 <mehdiziani1989@gmail.com>
2021-11-03 15:55:13 +0100

    Vue Panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,22 +3,46 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+
+  private $entityManager;
+
+  public function __construct(EntityManagerInterface $entityManager)
+  {
+    $this->entityManager = $entityManager;
+  }
+
   /**
    * @Route("/mon-panier", name="cart")
    */
   public function index(Cart $cart): Response
   {
+    $cartComplete = [];
+    
+    foreach ($cart->get() as $id => $quantity) {
+      $cartComplete[] = [
+        'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+        'quantity' => $quantity
+      ];
+    }
+
+
+
     return $this->render(
-      'cart/index.html.twig'
+      'cart/index.html.twig',
+      [
+        'cart' => $cartComplete
+      ]
     );
   }
-  
+
   /**
    * @Route("/cart/add/{id}", name="add_to_cart")
    */
